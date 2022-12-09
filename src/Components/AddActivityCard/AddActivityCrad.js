361ea545916b922f800eddc70acf50df2d8a8fac
pakajji <114389230+pakajji@users.noreply.github.com>
2022-12-09 17:05:02 +0700

    edit css

--- src/Components/AddActivityCard/AddActivityCrad.js
+++ src/Components/AddActivityCard/AddActivityCrad.js
@@ -2,7 +2,7 @@ import './AddActivityCard.css'
 
 const AddActivityCard = () => {
     return (
-        <a className="add-card" target="_blank" rel="noopener noreferrer" href='https://static01.nyt.com/images/2021/04/30/multimedia/30xp-meme/29xp-meme-mediumSquareAt3X-v5.jpg'>
+        <a className="add-activity-card" target="_blank" rel="noopener noreferrer" href='https://static01.nyt.com/images/2021/04/30/multimedia/30xp-meme/29xp-meme-mediumSquareAt3X-v5.jpg'>
             <button>+</button>
         </a>
     )
