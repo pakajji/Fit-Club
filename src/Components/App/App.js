a1a47f33a724658653cf7838ef3bf84c3244c074
pakajji <114389230+pakajji@users.noreply.github.com>
2022-12-10 08:14:05 +0700

    ...

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,7 @@ import Overview from "../Overview/Overview";
 import LoginForm from "../LoginForm/LoginForm";
 import RegisterForm from "../RegisterForm/RegisterForm";
 import ActivitiesForm from "../ActivitiesForm/ActivitiesForm";
+import PersonalProfile from "../PersonalProfile/PersonalProfile";
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<LoginForm/>}></Route>
         <Route path="/register" element={<RegisterForm/>}></Route>
         <Route path="/add-activity" element={<ActivitiesForm/>}></Route>
+        <Route path="/profile" element={<PersonalProfile/>}></Route>
       </Routes>
     </div>
   );
