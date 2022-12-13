c210d42060938e9318af29773cb3437f2fe94417
pakajji <114389230+pakajji@users.noreply.github.com>
2022-12-13 17:04:42 +0700

    activity card หรูหราหมาเห่า

--- src/Components/AllActivity/AllActivity.js
+++ src/Components/AllActivity/AllActivity.js
@@ -1,41 +1,33 @@
 import './AllActivity.css'
 import Activity from '../Activity/Activity'
 import AddActivityCard from '../AddActivityCard/AddActivityCrad'
-import axios from 'axios';
-import { useState , useEffect } from 'react'
+import { useContext } from 'react'
+import ActivityContext from '../Context/ActivityContext';
 
 const AllActivity = () => {
-    
-    const [activity, setActivity] = useState([])
-    
 
-    useEffect(() => async function getActivityCard() {
-        try {
-          const response = await axios.get('http://localhost:3000/activity');
-          await console.log(response.data)
-          await setActivity(response.data)
-        } catch (error) {
-          console.error(error);
-        }
-      }, [])
+    const { activities, isLoading, setShouldUpdate } = useContext(ActivityContext)
 
     return (
         <div>
             <h2 id="Dashboard" className="Activity_Summary">Activity Summary</h2>
             <div className="AllActivity">
-            {activity.map(detail => {
-                return <Activity type={detail.type}
-                                description={detail.description}
-                                date={detail.date}
-                                time={detail.time}
-                                numberset={detail.numberset}
-                                distance={detail.distance}
-                                calburned={detail.calburned}
-                                duration={detail.duration}
-                                heartrate={detail.heartrate}
-                                />  
-            })}
-            <AddActivityCard/>
+                {!isLoading && activities.map(detail => {
+                    return <Activity id={detail.id}
+                        key={detail.id}
+                        type={detail.type}
+                        description={detail.description}
+                        date={detail.date}
+                        time={detail.time}
+                        numberset={detail.numberset}
+                        distance={detail.distance}
+                        calburned={detail.calburned}
+                        duration={detail.duration}
+                        heartrate={detail.heartrate}
+                        onDelete={() => setShouldUpdate(true)}
+                    />
+                })}
+                <AddActivityCard />
             </div>
         </div>
     )
