594b1a9d4aacb540c1b0329aa750f83597eaf9de
pakajji <114389230+pakajji@users.noreply.github.com>
2022-12-17 23:59:01 +0700

    update main!!!

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,18 +5,28 @@ import LoginForm from "../LoginForm/LoginForm";
 import RegisterForm from "../RegisterForm/RegisterForm";
 import ActivitiesForm from "../ActivitiesForm/ActivitiesForm";
 import PersonalProfile from "../PersonalProfile/PersonalProfile";
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "../Main/index.jsx";
+import Signup from "../Signup/index.jsx";
+import Login from "../Login/index.jsx";
 
 function App() {
+  const user = localStorage.getItem("token");
+
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Overview/>}></Route>
-        <Route path="/login" element={<LoginForm/>}></Route>
-        <Route path="/register" element={<RegisterForm/>}></Route>
-        <Route path="/add-activity" element={<ActivitiesForm/>}></Route>
-        <Route path="/profile" element={<PersonalProfile/>}></Route>
+        {user && <Route path="/" exact element={<Main />} />}
+        <Route path="/signup" exact element={<Signup />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/" element={<Navigate replace to="/login" />} />
+
+        <Route path="/overview" exact element={<Overview />}></Route>
+        <Route path="/login2" element={<LoginForm />}></Route>
+        <Route path="/register" element={<RegisterForm />}></Route>
+        <Route path="/add-activity" element={<ActivitiesForm />}></Route>
+        <Route path="/profile" element={<PersonalProfile />}></Route>
       </Routes>
     </div>
   );
