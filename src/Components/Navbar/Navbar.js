aecac41ebd0de0366d12d99899ac0ab06691cc08
pakajji <114389230+pakajji@users.noreply.github.com>
2022-12-20 12:43:25 +0700

    แก้อะไรไม่รู้เยอะแยะตอน user ล็อกอินแล้ว

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,16 @@
 import './Navbar.css'
-import { useState } from 'react'
+import { useState , useContext } from 'react'
 import ProfliePopUp from '../ProfliePopUp/ProfliePopUp'
 import { Link } from 'react-router-dom'
+import UserContext from '../Context/UserContext'
 
 const Navbar = () => {
 
+    const { info } = useContext(UserContext)
+    console.log("info",info)
+
     //สลับปุ่ม Login กับ ProfilePic
-    const [logIn, setLogIn] = useState(false)
+    const [logIn, setLogIn] = useState(true)
     const showLogIn = logIn ? "hidden" : "show"
     let switchBtn = !logIn
     const showProfilePic = switchBtn ? "hidden" : "show"
@@ -28,16 +32,17 @@ const Navbar = () => {
                     <img className={`Profile_Pic_Ham ${showProfilePic}`} alt="Profile" src="https://cdn.shopify.com/s/files/1/0210/2968/3222/files/image17.jpg?v=1646429188"/>
                     <Link className={`nav-Login_btn ${showLogIn}`} to="/login">Log In</Link>
                 </li>
-                <li><a href="/#Dashboard">Dashboard</a></li>
-                <li><a href="/#Dietary">Dietary</a></li>
+                <li><a href="/overview">Overview</a></li>
+                <li><a href="/overview#Dashboard">Dashboard</a></li>
+                <li><a href="/overview#Dietary">Dietary</a></li>
             </ul>
         </div>
 
         <div id='notHamburger'>
             <ul>
-                <li className='nav-FitClub'><a href="/">Fit Club</a></li>
-                <li><a href="/#Dashboard">Dashboard</a></li>
-                <li><a href="/#Dietary">Dietary</a></li>
+                <li className='nav-FitClub'><a href="/overview">Fit Club</a></li>
+                <li><a href="/overview#Dashboard">Dashboard</a></li>
+                <li><a href="/overview#Dietary">Dietary</a></li>
             </ul>
             <img onClick={()=>setProfilePopUp(true)} className={`nav-Profile_Pic ${showProfilePic}`} alt="Profile" src="https://cdn.shopify.com/s/files/1/0210/2968/3222/files/image17.jpg?v=1646429188"/>
             <Link className={`nav-Login_btn ${showLogIn}`} to="/login">Log In</Link>
