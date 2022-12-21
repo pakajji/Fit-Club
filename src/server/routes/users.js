9a20ca39427de60f454d0a9450008f31cdb619e2
taikong <atai.tanapon@gmail.com>
2022-12-21 15:31:04 +0700

    Login system done

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { User, validate } = require("../Models/user");
+const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
+const config = require("../config");
 
 router.post("/", async (req, res) => {
   try {
@@ -14,7 +15,7 @@ router.post("/", async (req, res) => {
         .status(409)
         .send({ message: "User with given email already Exist!" });
 
-    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const salt = await bcrypt.genSalt(Number(config.salt));
     const hashPassword = await bcrypt.hash(req.body.password, salt);
 
     await new User({ ...req.body, password: hashPassword }).save();
@@ -24,37 +25,4 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
-  res.send({ message: "Hello, world!" });
-});
-
 module.exports = router;
-
-// require("dotenv").config();
-// const router = require("express").Router();
-// const { User, validate } = require("../Models/user");
-// const bcrypt = require("bcrypt");
-
-// router.post("/", async (req, res) => {
-//   try {
-//     const { error } = validate(req.body);
-//     if (error)
-//       return res.status(400).send({ message: error.details[0].message });
-
-//     const user = await User.findOne({ email: req.body.email });
-//     if (user)
-//       return res
-//         .status(409)
-//         .send({ message: "User with given email already Exist!" });
-
-//     const salt = await bcrypt.genSalt(Number(process.env.SALT));
-//     const hashPassword = await bcrypt.hash(req.body.password, salt);
-
-//     await new User({ ...req.body, password: hashPassword }).save();
-//     res.status(201).send({ message: "User created successfully" });
-//   } catch (error) {
-//     res.status(500).send({ message: "Internal Server Error" });
-//   }
-// });
-
-// module.exports = router;
